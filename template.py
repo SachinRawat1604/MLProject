974fbd60a9a91afda840b142f70a07f3d6780bff
SachinRawat1604 <sachinrawat4666@gmail.com>
2025-04-25 11:19:03 +0530

    Data Ingestion

--- template.py
+++ template.py
@@ -12,7 +12,7 @@ list_of_files=[
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
     f"src/{project_name}/components/model_tranier.py",
-    f"src/{project_name}/components/model_monitering",
+    f"src/{project_name}/components/model_monitering.py",
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipline.py",
     f"src/{project_name}/pipelines/prediction_pipeline.py",
